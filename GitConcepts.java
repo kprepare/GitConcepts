2f12517d1f98bfd557e3087c948aaff1daca6369
Ankit Srivastava <k.prepare@gmail.com>
2020-04-18 20:26:42 +0200

    added printPeacock method

--- GitConcepts.java
+++ GitConcepts.java
@@ -3,9 +3,14 @@ public class GitConcepts{
     public static void main(String[] args) {
         System.out.println("git concepts");
         printDuck();
+        printPeacock();
     }
 
     public static void printDuck(){
         System.out.println("Duck");
+    }
+
+    public static void printPeacock(){
+        System.out.println("Peacock");
     }
 }
