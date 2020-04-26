0a90bd5b036ffb012d83b865a25411eff1e762be
Ankit Srivastava <k.prepare@gmail.com>
2020-04-26 16:35:45 +0200

    Printing books

---
+++ BookPrinter.java
@@ -0,0 +1,9 @@
+
+public class BookPrinter {
+    public static void main(String[] args) {
+        printBooks();
+    }
+    public static void printBooks() {
+        System.out.println("Printing books......");
+    }
+}
