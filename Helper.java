9a80dbbd5ee213fc385ae33c3932cf858f00eb72
greenread <ankitprakash.ap@gmail.com>
2021-01-14 21:00:31 +0100

    Improve help message

--- Helper.java
+++ Helper.java
@@ -3,5 +3,6 @@ public class Helper {
     public static void printHelp() {
         System.out.println("Help for Git Conectps project.");
         System.out.println("This project is created for learning the concepts of git.");
+        System.out.println("and get stronger day by day in git.");
     }
 }
