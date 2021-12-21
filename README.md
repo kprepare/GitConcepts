6b8a48c2e6979a46ab1527a72278319be78b45fa
Ankit Srivastava <k.prepare@gmail.com>
2021-12-21 18:52:58 +0100

    Major release, now the project is managed via Maven

--- README.md
+++
@@ -1,2 +0,0 @@
-# GitConcepts
-User 2 is added and is able to make change in this repo
---
+++ src/main/java/com/supersoft/GitConcepts.java
@@ -0,0 +1,25 @@
+package com.supersoft;
+
+public class GitConcepts{
+    private static int i1;
+    private static Integer i2;
+    private static boolean b1;
+    private static Boolean b2;
+    
+    public static void main(String[] args) {
+        System.out.println(i1 + "," + i2 + "," + b1 + "," + b2);
+       
+    }
+
+    public static void printDuck(){
+        System.out.println("Duck");
+    }
+
+    public static void printPeacock(){
+        System.out.println("Peacock");
+    }
+
+    public static void printBird(){
+        System.out.println("Bird");
+    }
+}
